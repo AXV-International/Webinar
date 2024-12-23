15867907f3a4d9268a42faca528f801be33ad075
Zero <zeroxtwoid@gmail.com>
2024-12-23 23:41:55 +0700

    Add files via upload

---
+++ Menghadapi Teknologi Kuantum : Peluang serta Tantangan dalam Kriptografi dan Keamanan Informasi/chall.py
@@ -0,0 +1,203 @@
+a = input(
+'''
+Apa faktor pembeda antara teknologi klasik dan teknologi kuantum?
+a. Qubit
+b. Warna
+c. Daya Listrik
+d. Tidak ada Perbedaan
+    
+Answer :
+'''
+)
+if a == 'a':
+    print('Result = Benar')
+else:
+    print('Result = Salah')
+
+
+b = input(
+'''
+Berikut ini yang bukan merupakan jenis teknologi qubit pada komputer kuantum adalah
+a. Superconducting Qubit
+b. Trapped Ion Qubits
+c. Photons
+d. QKD
+    
+Answer : 
+'''
+)
+if b == 'd':
+    print('Result = Benar')
+else:
+    print('Result = Salah')
+    
+    
+c = input(
+'''
+Contoh platform SDK yang dapat digunakan untuk pemrograman dalam komputasi kuantum adalah
+a. Qiskit
+b. Java Web Application
+c. Pascal
+d. Qubit
+    
+Answer : 
+'''
+)
+if c == 'a':
+    print('Result = Benar')
+else:
+    print('Result = Salah')
+    
+    
+d = input(
+'''
+Berikut ini adalah contoh bidang dalam implementasi teknologi kuantum, kecuali
+a. Quantum Sensing
+b. Algoritma dan Aplikasi
+c. Mengoptimalkan Rute Kendaraan
+d. Memecah Atom Kimia
+    
+Answer : 
+'''
+)
+if d == 'c':
+    print('Result = Benar')
+else:
+    print('Result = Salah')
+    
+    
+e = input(
+'''
+Nama satuan basis komputasi pada komputer kuantum
+a. Quantum Leap
+b. Quantum Bit
+c. Quantum Channel
+d. Quantum Chip
+    
+Answer : 
+'''
+)
+if e == 'b':
+    print('Result = Benar')
+else:
+    print('Result = Salah')
+    
+    
+f = input(
+'''
+Berikut adalah beberapa kelas algoritma Post Quantum Cryptography, kecuali
+a. Hash Based Cryptography
+b. Code Based Cryptography
+c. Lattice Based Cryptography
+d. Elliptic Curve Based Cryptography
+    
+Answer : 
+'''
+)
+if f == 'd':
+    print('Result = Benar')
+else:
+    print('Result = Salah')
+    
+    
+g = input(
+'''
+Serangan pada Keamanan Siber sebagai dampak perkembangan Komputer Kuantum adalah  
+a. Man in The Middle Attack
+b. Encrypt then Sign
+c. Harvest now, Decrypt later
+d. Forgery Attack
+    
+Answer : 
+'''
+)
+if g == 'c':
+    print('Result = Benar')
+else:
+    print('Result = Salah')
+    
+    
+h = input(
+'''
+Berikut adalah beberapa algoritma kuantum, kecuali  
+a. Algoritma Shor
+b. Algoritma Simon
+c. Algoritma Grover
+d. Algoritma Matsui
+    
+Answer : 
+'''
+)
+if h == 'd':
+    print('Result = Benar')
+else:
+    print('Result = Salah')
+    
+    
+i = input(
+'''
+Algoritma berikut adalah bagian dari standar NIST PQC, kecuali 
+a. RSA
+b. Kyber
+c. Falcon
+d. Dilithium
+    
+Answer : 
+'''
+)
+if i == 'a':
+    print('Result = Benar')
+else:
+    print('Result = Salah')
+    
+    
+j = input(
+'''
+Berdasarkan perkiraan, kapan batas waktu migrasi ke PQC menurut sumber NASEM?
+a. 2025
+b. 2030
+c. 2035
+d. 2040
+    
+Answer : 
+'''
+)
+if j == 'c':
+    print('Result = Benar')
+else:
+    print('Result = Salah')
+    
+    
+k = input(
+'''
+Apa nama solusi sementara untuk migrasi infrastruktur yang rentan terhadap serangan kuantum tanpa akses ke kode sumber?  
+a. QCore
+b. QTunnel
+c. QVision
+d. Secure Key Integration Protocol (SKIP)
+    
+Answer : 
+'''
+)
+if k == 'b':
+    print('Result = Benar')
+else:
+    print('Result = Salah')
+    
+    
+l = input(
+'''
+Kapan NIST mengumumkan standar PQC yang diterbitkan, termasuk ML-KEM FIPS 203 dan ML-DSA FIPS 204?  
+a. Februari 2016
+b. Juli 2020
+c. Juli 2022
+d. Agustus 2024
+    
+Answer : 
+'''
+)
+if l == 'd':
+    print('Result = Benar')
+else:
+    print('Result = Salah')
+    
